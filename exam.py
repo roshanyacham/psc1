08fbe30369cf867c01d5fda66609d19aad1d81e5
roshanyacham <roshanyacham301@gmail.com>
2024-04-17 15:53:14 +0530

    added error template

--- exam.py
+++ exam.py
@@ -150,6 +150,21 @@ dashboard_template = '''
 </body>
 </html>
 '''
+
+error_template = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error</title>
+</head>
+<body>
+    <h1>Error</h1>
+    <p>{message}</p>
+</body>
+</html>
+'''
 # Database connection initialization and functions
 
 def connect_to_db():
